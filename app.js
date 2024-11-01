b2e30980ad5856387cec03c77ff33d0764b2b723
Your Name <karanxhagiulia@gmail.com>
2024-11-01 17:20:49 +0100

    updates

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+// Imports and the setup, using expressm fs for reading and writing
 const express = require('express');
 const fs = require('fs');
 const path = require('path');
@@ -6,13 +7,13 @@ const bodyParser = require('body-parser');
 const app = express();
 const PORT = 3000;
 
-// Middleware
+// Using bodyParser for parsing incoming request 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 app.set('view engine', 'ejs');
 
-// Utility function to load messages safely
+// Utility function to load messages and returns and empty array if it's not read correctly
 function loadMessages() {
     try {
         const data = fs.readFileSync('messages.json', 'utf8');
@@ -42,6 +43,9 @@ app.get('/ajaxmessage', (req, res) => {
 });
 
 // Post route for /newmessage
+//Validates all the required fields so everything is filled
+//reads existing messages, adds the new messages, saves the updated list back to messages.json (if the saving doesn't work it will give an error)
+//Redirects to guestbook once the message is submitted correctly
 app.post('/newmessage', (req, res) => {
     const { username, country, message } = req.body;
     if (!username || !country || !message) {
@@ -66,6 +70,7 @@ app.post('/newmessage', (req, res) => {
 });
 
 // AJAX route for /ajaxmessage
+//Similar to previous but it will make a request to the server without reloading the page
 app.post('/ajaxmessage', (req, res) => {
     //basic validation to ensure all fields are filled
     const { username, country, message } = req.body;
